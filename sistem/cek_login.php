4fed95c70870fd1a982688c929a4a005f0c69448
EVANS ZULHENDY <evanszul@gmail.com>
2017-07-25 20:24:35 +0700

    ubah cek login

--- sistem/cek_login.php
+++ sistem/cek_login.php
@@ -6,8 +6,13 @@ session_start();
 
 $userlogin = $_SESSION['username'];
 
-$query = mysqli_query($koneksi,"SELECT * FROM tbl_user where username='$userlogin'");
-$data_user = mysqli_fetch_assoc($query);
+if($userlogin == ""){
+    header("location:login.php");
+} else{
 
+    $query = mysqli_query($koneksi,"SELECT * FROM tbl_user where username='$userlogin'");
+    $data_user = mysqli_fetch_assoc($query);
+
+}
 
 ?>
