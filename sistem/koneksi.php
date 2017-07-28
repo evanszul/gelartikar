b7b52727b86484abdbbb57647c4d3d816af414be
EVANS ZULHENDY <evanszul@gmail.com>
2017-07-28 16:56:53 +0700

    balik ke db localhost

--- sistem/koneksi.php
+++ sistem/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'sql12.freemysqlhosting.net';
-$user = 'sql12187229';
-$pass = 'nitDPFxyxb';
-$database = 'sql12187229';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$database = 'gelartikar';
 
 $koneksi = mysqli_connect($host,$user,$pass,$database);
 if (!$koneksi){
