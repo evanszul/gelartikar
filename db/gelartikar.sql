f0ff94c2ee56731bcd30ba911586b904e4d0fe28
EVANS ZULHENDY <evanszul@gmail.com>
2017-07-29 12:36:07 +0700

    ok

--- db/gelartikar.sql
+++ db/gelartikar.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 19 Jul 2017 pada 20.09
--- Versi Server: 5.6.24
+-- Generation Time: Jul 29, 2017 at 07:35 AM
+-- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_admin`
+-- Table structure for table `tbl_admin`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_admin` (
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `tbl_admin` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_alamat_user`
+-- Table structure for table `tbl_alamat_user`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_alamat_user` (
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `tbl_alamat_user` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_barang`
+-- Table structure for table `tbl_barang`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_barang` (
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `tbl_barang` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_brand`
+-- Table structure for table `tbl_brand`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_brand` (
@@ -83,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `tbl_brand` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_kategori_barang`
+-- Table structure for table `tbl_kategori_barang`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_kategori_barang` (
@@ -97,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `tbl_kategori_barang` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_rekening_user`
+-- Table structure for table `tbl_rekening_user`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_rekening_user` (
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `tbl_rekening_user` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_transaksi`
+-- Table structure for table `tbl_transaksi`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_transaksi` (
@@ -131,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `tbl_transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_transaksi_barang`
+-- Table structure for table `tbl_transaksi_barang`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_transaksi_barang` (
@@ -145,7 +145,7 @@ CREATE TABLE IF NOT EXISTS `tbl_transaksi_barang` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_transfer`
+-- Table structure for table `tbl_transfer`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_transfer` (
@@ -163,7 +163,7 @@ CREATE TABLE IF NOT EXISTS `tbl_transfer` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_ukuran`
+-- Table structure for table `tbl_ukuran`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_ukuran` (
@@ -176,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `tbl_ukuran` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_user`
+-- Table structure for table `tbl_user`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_user` (
@@ -185,17 +185,20 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `username` varchar(30) NOT NULL,
   `pwd_user` varchar(100) NOT NULL,
   `email_user` varchar(50) NOT NULL,
-  `tlp_user` int(100) NOT NULL,
+  `tlp_user` bigint(250) NOT NULL,
   `catatan_user` varchar(250) NOT NULL,
   `status_user` varchar(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tbl_user`
+-- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `pwd_user`, `email_user`, `tlp_user`, `catatan_user`, `status_user`) VALUES
-(1, 'Evans Zulhendy', 'evanszul', 'bukutulis', 'evanszul@gmail.com', 2147483647, '', '');
+(3, 'Evans Zulhendy', 'evanszul', '$2y$10$ql.lt0y8hy236JFsN7uzW.zEhpkvnNAbXvnEKdmGFaLv0gdsat3b.', 'evanszul@gmail.com', 2147483647, '', ''),
+(4, 'Kevin Ismail', 'kevinmail', '$2y$10$KcyNMP3.dw9X1XA4fEIyiuqWhVIEYe8rXIFXO1W0RmJqDpNBTD1w6', 'kevin@gmail.com', 2147483647, '', ''),
+(5, 'Rio Juliansyah', 'rio', '$2y$10$bJO3qCT910aC1niqWEawZ.NqIWum8ZZUFKEdkMpfY/O8YBYnRokFm', 'rio.minang@gmail.com', 2147483647, '', ''),
+(6, 'Oklan', 'ocland', '$2y$10$IqdNHIY/LmxynlhCuQVqwu8F9P498Ukxlvtk/.ezoeFWw24CWw04G', 'ocland@mail.com', 81261202102, '', '');
 
 --
 -- Indexes for dumped tables
@@ -218,6 +221,12 @@ ALTER TABLE `tbl_alamat_user`
 --
 ALTER TABLE `tbl_barang`
   ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `tbl_brand`
+--
+ALTER TABLE `tbl_brand`
+  ADD PRIMARY KEY (`id_brand`);
 
 --
 -- Indexes for table `tbl_kategori_barang`
@@ -281,6 +290,11 @@ ALTER TABLE `tbl_alamat_user`
 ALTER TABLE `tbl_barang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `tbl_brand`
+--
+ALTER TABLE `tbl_brand`
+  MODIFY `id_brand` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `tbl_kategori_barang`
 --
 ALTER TABLE `tbl_kategori_barang`
@@ -314,7 +328,7 @@ ALTER TABLE `tbl_ukuran`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
